92cb1c8abc2f3601949600d157c94fb7e08ab572
karimamnr <karimamnr130@gmail.com>
2024-09-14 19:26:46 +0200

    XO game

---
+++ XO-game/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import  XOgame  from './XO/XOgame'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <XOgame/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
